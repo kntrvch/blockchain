572c01c7172edc16bd4a7dc8949f48d08c5e256d
kntrvch <kntrvch@gmail.com>
2018-12-24 03:17:45 +0100

    nodemon, api endpoints

--- dev/api.js
+++ dev/api.js
@@ -1,18 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/blockchain', function (req, res) {
-
+    res.send('/blockchain endpoint reached');
 });
 
-app.post('/transaction', function(req, res) {
-
+app.post('/transaction', function (req, res) {
+    console.log(req.body);
+    res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
 });
 
-app.get('/mine', function(req, res) {
-
+app.get('/mine', function (req, res) {
+    res.send('/mine endpoint reached');
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('listening on port 3000...');
 });
