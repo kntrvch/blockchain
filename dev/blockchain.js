aabd512825456e720b9072e6d9f03798cd6883f0
kntrvch <kntrvch@gmail.com>
2018-12-29 00:57:01 +0100

    chain valid method / cleanup

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -5,30 +5,35 @@ const uuid = require('uuid/v1');
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
+
     this.currentNodeUrl = currentNodeUrl;
     this.networkNodes = [];
-    this.createNewBlock(100, '0', '0');
-}
 
-Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) { 
+    this.createNewBlock(100, '0', '0');
+};
+
+
+Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
-        timestamp: Date.now(), 
+        timestamp: Date.now(),
         transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
     };
 
-    this.newTransaction = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
-    return newBlock; 
-}
+    return newBlock;
+};
 
-Blockchain.prototype.getLastBlock = function () { 
+
+Blockchain.prototype.getLastBlock = function () {
     return this.chain[this.chain.length - 1];
-}
+};
+
 
 Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
     const newTransaction = {
@@ -37,29 +42,74 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
         recipient: recipient,
         transactionId: uuid().split('-').join('')
     };
-    return newTransaction;
-}
 
-Blockchain.prototype.addTransactionToPendingTransactions = function(transactionObj) {
+    return newTransaction;
+};
+
+
+Blockchain.prototype.addTransactionToPendingTransactions = function (transactionObj) {
     this.pendingTransactions.push(transactionObj);
     return this.getLastBlock()['index'] + 1;
 };
 
-Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
     const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
     const hash = sha256(dataAsString);
     return hash;
-}
+};
 
-Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+
+Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
     let nonce = 0;
-    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce); 
-    while (hash.substring(0, 4) !== '0000') {        
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0, 4) !== '0000') {
         nonce++;
         hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
         console.log(hash);
     }
+
     return nonce;
-}
+};
+
+
+
+Blockchain.prototype.chainIsValid = function (blockchain) {
+    let validChain = true;
+
+    for (var i = 1; i < blockchain.length; i++) {
+        const currentBlock = blockchain[i];
+        const prevBlock = blockchain[i - 1];
+        const blockHash = this.hashBlock(prevBlock['hash'], { transactions: currentBlock['transactions'], index: currentBlock['index'] }, currentBlock['nonce']);
+        if (blockHash.substring(0, 4) !== '0000') validChain = false;
+        if (currentBlock['previousBlockHash'] !== prevBlock['hash']) validChain = false;
+        console.log('previousBlockHash =>', prevBlock [ 'hash']);
+        console.log('currentBlockHash =>', currentBlock [ 'hash']);
+    };
+
+    const genesisBlock = blockchain[0];
+    const correctNonce = genesisBlock['nonce'] === 100;
+    const correctPreviousBlockHash = genesisBlock['previousBlockHash'] === '0';
+    const correctHash = genesisBlock['hash'] === '0';
+    const correctTransactions = genesisBlock['transactions'].length === 0;
+
+    if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) validChain = false;
+
+    return validChain;
+};
+
 
 module.exports = Blockchain;
+
+
+
+
+
+
+
+
+
+
+
+
+
